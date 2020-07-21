4b63614635e32357771fdfb5b3b003094fd6c746
justahuman1 <saivdev@protonmail.com>
2020-07-20 17:27:44 -0700

    Added UI Cache Management & Menu

--- rofi/manager.sh
+++ rofi/manager.sh
@@ -14,17 +14,23 @@ export TXT="#bdc3c3"                    # Text color
 export GRID_FONT="Iosevka 13"           # Font (Include size)
 export ACTIVE="rgba(9, 145, 224, 0.4)"  # Active Cell background
 export SBAR="#242222"                   # Search Bar background
+export WIDTH="220px"                    # Width of rofi widget
 
 ## FIXME: Path to the src python file/module
-# grid_src="-m i3grid"
+grid_src="-m i3grid"
 # Uncomment below line if cloned from github
-grid_src="../i3-grid/i3grid"
+# grid_src="../i3-grid/i3grid"
+# The python interpreter to use
+PY_ENV="/usr/bin/python3"
 
 # Initalize
 join() { local IFS="$1"; shift; echo "$*"; }
 # Absolute path to script (must be in the same folder as matrix.rasi)
 app_abs_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rofi_command="rofi -i -theme $app_abs_path/matrix.rasi -multi-select"
+cache_file="$app_abs_path/grid.cache"
+[ -f "$cache_file" ] || touch "$cache_file"  # Make cache file if not exists
+RUN_COMMAND="$PY_ENV $grid_src"
 # We use a bash function to dynamically generate the
 # grid for different canvases on the fly.
 grid=()
@@ -52,11 +58,12 @@ grid+=(
   "G" # Guake style window
   "H" # Hide all workspace floating windows, if scratchpad
   "R" # Reset to i3 default center (75% screen)
+  "RR" # ReRun a cached command (drop down option)
   "SF" # Snaps all current *floating windows into a grid
   "X" # Custom col, row, target parsing
   "P"
 
-  ## FIXME: Add command here. Define it's call in the case statement below (line 125).
+  ## FIXME: Add command here. Define it's call in the case statement below (line 136).
 )
 options=""
 for i in ${grid[*]}; do
@@ -69,73 +76,74 @@ if [[ "$len" -gt "2" ]]; then  # if multi select
   declare -a arr
   arr=( $(echo $chosen | awk '{split($0,a," ")} END {for(n in a){ print a[n] }}') )
   multi_arg=$(join ' ' ${arr[@]})
-  python $grid_src multi --multis $multi_arg
-  exit
+  echo "$RUN_COMMAND multi --cols $COLS --rows $LINES --multis $multi_arg" >> $cache_file
 elif [[ ! " ${grid[@]} " =~ " ${chosen} " ]]; then
     # Catch non-grid user input
     # Ex: I run raw custom commands that I type into the grid (as outputted by rofi)
     echo "Error: Element Out of grid"
-    python $grid_src -h
+    $RUN_COMMAND -h
     exit
+else
+  # Grid options
+  case "$chosen" in
+  "A")
+    echo "$RUN_COMMAND snap --all" >> $cache_file
+  ;;
+  "C")
+    p="$($rofi_command -dmenu -p "% -" -selected-row 0)"
+    [ -z "$p" ] && exit || echo "$RUN_COMMAND csize --perc=$p" >> $cache_file
+  ;;
+  "D")
+    target="$($rofi_command -dmenu -p 'Target:' -selected-row 0)"
+    echo "$RUN_COMMAND snap --target $target" >> $cache_file
+  ;;
+  "F")
+    echo "$RUN_COMMAND csize --perc 100" >> $cache_file
+  ;;
+  "G")
+     echo "$RUN_COMMAND snap --target 1 --rows 2 --cols 1 --offset 0 80 0 80" >> $cache_file
+  ;;
+  "H")
+    echo "$RUN_COMMAND hide --noresize --nofloat --all" >> $cache_file
+  ;;
+  "R")
+    echo "$RUN_COMMAND reset" >> $cache_file
+  ;;
+  "RR")
+    WIDTH="380px"
+    IFS=
+    data=$( sort $cache_file | uniq | awk 'NF' | awk '{out=$4; for(i=5;i<=NF;i++){out=out" "$i}; print out}' )
+    custom=$( (echo $data) | rofi -i -theme $app_abs_path/matrix.rasi -multi-select -dmenu -p "Cached: ")
+    [ -z "$custom" ] && exit || eval "${RUN_COMMAND} ${custom}"  # Run command if not blank else exit
+  ;;
+  "P")
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    OUTPUT=$("$DIR/i3classifier.sh")
+    echo "$RUN_COMMAND snap --filter "$OUTPUT"" >> $cache_file
+  ;;
+  "SF")
+    echo "$RUN_COMMAND snap --floating --rows 3 --cols 2" >> $cache_file
+  ;;
+  "X")
+    custom="$($rofi_command -dmenu -p "c r t:" -selected-row 0)"
+    IFS=' ' read -ra r_c_t <<< "$custom"
+    if [[ "${#r_c_t[@]}" != "3" ]]; then
+      echo "Incorrect Argument Length (Requires Row, Col, Target)"
+      exit
+    fi
+    echo "$RUN_COMMAND snap --cols ${r_c_t[0]} --rows ${r_c_t[1]} --target ${r_c_t[2]}" >> $cache_file
+  ;;
+  ## FIXME Define custom calls here. Template (Ex: Using custom command 'O'):
+  # "O")
+  #   echo "$RUN_COMMAND <action> <optional-flags>" >> $cache_file
+  # ;;
+  *)
+    # Any number outside the grid can be used as a custom percentage resize
+    echo "$RUN_COMMAND csize --perc $chosen" >> $cache_file
+    # Uncomment to see the raw command sent to i3-grid
+    # echo "$RUN_COMMAND csize --perc $chosen"
+  ;;
+  esac
 fi
-# Non multi options
-# NOTE: These settings are not optimized. The resize function (& others)
-#       may be run multiple times if autoresize is also on. Make
-#       sure to update the functions in accordance to your config
-#       for optimal perfomance (<50ms actions).
-case "$chosen" in
-"A")
-  python $grid_src snap --all
-;;
-"C")
-  p="$($rofi_command -dmenu -p "% -" -selected-row 0)"
-  python $grid_src csize --perc=$p
-;;
-"D")
-  target="$($rofi_command -dmenu -p 'Target:' -selected-row 0)"
-  python $grid_src snap --target $target
-;;
-"F")
-  python $grid_src csize --perc 100
-;;
-"G")
-   python $grid_src snap --target 1 --rows 2 --cols 1 --offset 0 80 0 80
-;;
-"H")
-  python $grid_src hide --noresize --nofloat --all
-;;
-"R")
-  python $grid_src reset
-;;
-"P")
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  OUTPUT=$("$DIR/i3classifier.sh")
-  python $grid_src snap --filter "$OUTPUT"
-;;
-"SF")
-  python $grid_src snap --floating --rows 3 --cols 2
-;;
-"X")
-  custom="$($rofi_command -dmenu -p "c r t:" -selected-row 0)"
-  IFS=' ' read -ra r_c_t <<< "$custom"
-  if [[ "${#r_c_t[@]}" != "3" ]]; then
-    echo "Incorrect Argument Length (Need Row, Col, Target)"
-    exit
-  fi
-  python $grid_src \
-    snap  \
-    --cols ${r_c_t[0]} --rows ${r_c_t[1]}  \
-    --target ${r_c_t[2]}
-;;
-
-## FIXME Define custom calls here. Template (Ex: Using custom command 'O'):
-# "O")
-#   python $grid_src <action> <optional-flags>
-# ;;
-
-*)
-  # Uncomment to see the raw command sent to i3-grid
-  # echo "python $grid_src float snap --cols $COLS --rows $LINES --target $chosen"
-  python $grid_src snap --cols $COLS --rows $LINES --target $chosen
-;;
-esac
+# Run final command (latest)
+$(tail -n 1 $cache_file)
